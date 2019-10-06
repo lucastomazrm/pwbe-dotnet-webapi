889f6848b0d6642f4ce0540f3f97d8d232979c3e
Lucas Tomaz Resende Miranda <lucas.rmiranda@ibm.com>
2019-10-06 20:18:38 -0300

    build tentative

--- Web/Controllers/HealthyController.cs
+++ Web/Controllers/HealthyController.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 
 namespace PWBE.TeamAthlete.Web.Controllers
 {
     public class HealthyController : Controller
     {
         [AllowAnonymous]
-        public async Task<string> Index()
+        public IEnumerable<string> Index()
         {
-            return "Ok";
+            string dirPath = Directory.GetCurrentDirectory();
+            List<string> files = new List<string>();
+            DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
+            foreach (FileInfo fInfo in dirInfo.GetFiles())
+            {
+                files.Add(fInfo.Name);
+            }
+            return files.ToArray();
         }
     }
 }
