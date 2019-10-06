40b890ced2797d9c0318b9169afa7806b39d8b93
Lucas Tomaz Resende Miranda <lucas.rmiranda@ibm.com>
2019-10-06 16:11:05 -0300

    healthy controller

---
+++ Web/Controllers/HomeController copy.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PWBE.TeamAthlete.Web.Controllers
+{
+    public class HealthyController : Controller
+    {
+        [AllowAnonymous]
+        public async Task<string> Index()
+        {
+            return "Ok";
+        }
+    }
+}
